a139064fd957ac965bc9ed7155876ecd1a0da5ae
Anifatul Aufah <anifatulaufah09@gmail.com>
2018-05-25 16:10:33 +0700

    tambah method delete

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -117,6 +117,8 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect()->route('posts.index');
     }
 }
